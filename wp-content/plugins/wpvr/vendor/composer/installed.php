b781961e931134bfa2556fd54c9ae7f51223a865
/usr/bin/vim <support@beget.ru>
2023-02-03 14:17:32 +0300

    3d tour

---
+++ wp-content/plugins/wpvr/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4387f4ae9dd439a4a8dff43547b241cd7b32c4cc',
+        'name' => 'wpvr/wpvr',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpvr/wpvr' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4387f4ae9dd439a4a8dff43547b241cd7b32c4cc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
